23b9f8d1c531e04207659123fe83a1311b7bf633
Pedro Henrique da Silva <78162685+PedroHS-BR@users.noreply.github.com>
2025-05-01 11:58:13 -0300

    Initial Setup

---
+++ src/main/java/com/hidra/bitcoingold/BitcoinGoldApplication.java
@@ -0,0 +1,13 @@
+package com.hidra.bitcoingold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitcoinGoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitcoinGoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hidra/bitcoingold/BitcoinGoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidra.bitcoingold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinGoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
