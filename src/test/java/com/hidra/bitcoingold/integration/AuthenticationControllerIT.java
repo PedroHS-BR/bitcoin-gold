ae87c2eeb47be48b6c8e82384d7ea37c7e276a7c
Pedro Henrique da Silva <78162685+PedroHS-BR@users.noreply.github.com>
2025-07-28 21:23:28 -0300

    teste de integração AuthenticationController

---
+++ src/test/java/com/hidra/bitcoingold/integration/AuthenticationControllerIT.java
@@ -0,0 +1,112 @@
+package com.hidra.bitcoingold.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hidra.bitcoingold.domain.User;
+import com.hidra.bitcoingold.domain.UserRole;
+import com.hidra.bitcoingold.dtos.user.RegisterUserPostRequest;
+import com.hidra.bitcoingold.dtos.user.UserLoginRequest;
+import com.hidra.bitcoingold.exception.BadRequestException;
+import com.hidra.bitcoingold.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest(properties = "spring.profiles.active=test")
+public class AuthenticationControllerIT {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void testConnect() throws Exception {
+        mockMvc.perform(get("/auth/connect"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Connecting to Bitcoin"));
+    }
+
+    @Test
+    void register_ShouldCreateNewUser() throws Exception {
+        RegisterUserPostRequest request = new RegisterUserPostRequest(
+                "Pedro", "pedro@gmail.com", "123456789");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.name").value("Pedro"))
+                .andExpect(jsonPath("$.email").value("pedro@gmail.com"))
+                .andExpect(jsonPath("$.role").value("USER"));
+
+        User user = userRepository.findByEmail("pedro@gmail.com")
+                .orElseThrow(() -> new BadRequestException("User not found"));
+        Assertions.assertEquals("Pedro", user.getName());
+        Assertions.assertEquals("pedro@gmail.com", user.getEmail());
+        Assertions.assertTrue(passwordEncoder.matches("123456789", user.getPassword()));
+        Assertions.assertEquals(UserRole.USER, user.getRole());
+    }
+
+    @Test
+    void login_ShouldBeSuccessful() throws Exception {
+        RegisterUserPostRequest request = new RegisterUserPostRequest(
+                "Pedro", "pedro@gmail.com", "123456789");
+
+        UserLoginRequest userLoginRequest = new UserLoginRequest(
+                "pedro@gmail.com", "123456789"
+        );
+
+        mockMvc.perform(post("/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+        .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").isNotEmpty());
+    }
+
+    @Test
+    void login_ShouldNotBeSuccessful() throws Exception {
+        RegisterUserPostRequest request = new RegisterUserPostRequest(
+                "Pedro", "pedro@gmail.com", "123456789");
+
+        UserLoginRequest userLoginRequest = new UserLoginRequest(
+                "pedro@gmail.com", "123"
+        );
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userLoginRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.details").value("Invalid email or password"));
+    }
+
+}
